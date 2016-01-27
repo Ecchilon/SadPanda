850c588a5a4f3037a49ef670a18e52e6d24a0301
Ecchilon <ecchilon@gmail.com>
2016-01-27 21:16:42 +0100

    Added OkHttp caching

--- app/src/main/java/com/ecchilon/sadpanda/ExhentaiModule.java
+++ app/src/main/java/com/ecchilon/sadpanda/ExhentaiModule.java
@@ -1,18 +1,19 @@
 package com.ecchilon.sadpanda;
 
+import java.io.File;
+
 import android.app.Application;
 import android.content.Context;
+import android.os.Environment;
 import com.ecchilon.sadpanda.auth.ExhentaiAuth;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.squareup.picasso.Picasso;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 
-/**
- * Created by Alex on 20-9-2014.
- */
 public class ExhentaiModule extends AbstractModule {
 
 	@Inject
@@ -34,7 +35,20 @@ public class ExhentaiModule extends AbstractModule {
 
 	@Provides
 	@Singleton
-	private OkHttpClient client() {
-		return new OkHttpClient();
+	private OkHttpClient client(Cache cache) {
+		return new OkHttpClient.Builder().cache(cache).build();
+	}
+
+	@Provides
+	private Cache cache(Context context) {
+		File cacheDir;
+		if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+			cacheDir = context.getExternalCacheDir();
+		}
+		else {
+			cacheDir = context.getCacheDir();
+		}
+		int cacheSize = 10 * 1024 * 1024;
+		return new Cache(new File(cacheDir, "http"), cacheSize);
 	}
 }
